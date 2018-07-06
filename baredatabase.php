2c6328c0b060df6e2f2239a37dca2ce60a1f28ab
chaithanya.n <chaithanya.neelam@rivigroup.net>
2018-07-06 13:04:27 +0530

    modified file

--- baredatabase.php
+++ baredatabase.php
@@ -5,8 +5,6 @@ $connection = mysqli_connect("localhost","root","");
 
 // 2. Select a database to use 
 $db_select = mysqli_select_db($connection, "sample");
-if (!$db_select) {
-    die("Database selection failed: " . mysqli_error($connection));
-}
+
 ?>
 
