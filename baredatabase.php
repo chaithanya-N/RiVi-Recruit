0f620ca583682191dfdade39e50dabbc4a123764
chaithanya.n <chaithanya.neelam@rivigroup.net>
2018-07-06 12:56:04 +0530

    first commit

--- baredatabase.php
+++ baredatabase.php
@@ -1,9 +1,7 @@
 <?php
 // 1. Create a database connection
 $connection = mysqli_connect("localhost","root","");
-if (!$connection) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
+
 
 // 2. Select a database to use 
 $db_select = mysqli_select_db($connection, "sample");
